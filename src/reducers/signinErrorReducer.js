5094e8ca9895d0339015d616031fefd88b1d5d0e
Akash Sharma <lucky1612000@gmail.com>
2021-06-13 20:13:39 +0530

    Added more robust error handling to errorreducers.

--- src/reducers/signinErrorReducer.js
+++ src/reducers/signinErrorReducer.js
@@ -6,8 +6,11 @@ const signinErrorReducer = (state = null, action) => {
 	switch (action.type) {
 		case SIGN_IN + vars.r:
 			if (action.payload.response)
-				return action.payload.response.data;
-			return "Cannot connect to the server."
+				if(typeof action.payload.response !== "string")
+					return "Internal Server Error";
+				else
+					return action.payload.response.data;
+			return "Cannot connect to the server"
 		default:
 			return state;
 	}
--- src/reducers/signupErrorReducer.js
+++ src/reducers/signupErrorReducer.js
@@ -5,9 +5,12 @@ import { vars } from './commonData';
 const signupErrorReducer = (state = null, action) => {
 	switch (action.type) {
 		case SIGN_UP + vars.r:
-            if(action.payload.response)
-			    return action.payload.response.data;
-            return "Cannot connect to the server."
+			if (action.payload.response)
+				if (typeof action.payload.response !== "string")
+					return "Internal Server Error";
+				else
+					return action.payload.response.data;
+			return "Cannot connect to the server"
 		default:
 			return state;
 	}
