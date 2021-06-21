ec59380d437f1cb25f953707ae7e1385789b5da6
Akash Sharma <lucky1612000@gmail.com>
2021-06-22 01:36:51 +0530

    final remote urls

--- src/actions/commonData.js
+++ src/actions/commonData.js
@@ -1,2 +1,2 @@
 export const user_remote = "http://ec2-3-108-41-178.ap-south-1.compute.amazonaws.com:9903";
-export const product_remote = "ec2-13-233-62-113.ap-south-1.compute.amazonaws.com:9902";
+export const product_remote = "http://ec2-13-233-62-113.ap-south-1.compute.amazonaws.com:9902";
